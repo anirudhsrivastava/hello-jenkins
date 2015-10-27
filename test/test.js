fa8056e2324256d7316595bf2038f635a083d03a
Anirudh Srivastava <AnirudhSrivastava@Manu-MacBook-Pro.local>
2015-10-27 19:39:27 +0530

    Changed test.js to hello jenkins 2

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
 	it ('request with hello world', function(done) {
-		request(app).get('/').expect('hello world', done);
+		request(app).get('/').expect('hello jenkins 2', done);
 	});
 });
